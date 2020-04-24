a9005f1cb55ba6311567a974bfb94053ff3d0d64
Robert Berger <rberger@ibd.com>
2020-04-24 00:16:00 -0700

    Need to import only aws-amplify/core to not get the http2 error

--- src/omnyway/mock_checkout_page/core.cljs
+++ src/omnyway/mock_checkout_page/core.cljs
@@ -1,23 +1,32 @@
 (ns omnyway.mock-checkout-page.core
-    (:require
-     [reagent.core :as reagent]
-     [re-frame.core :as re-frame]
-     [omnyway.mock-checkout-page.events :as events]
-     [omnyway.mock-checkout-page.views :as views]
-     [omnyway.mock-checkout-page.config :as config]
-     ))
+  (:require
+   [reagent.core :as reagent]
+   [re-frame.core :as re-frame]
+   [omnyway.mock-checkout-page.events :as events]
+   [omnyway.mock-checkout-page.views :as views]
+   [omnyway.mock-checkout-page.config :as config]
+   ["@aws-amplify/core" :default Amplify :as amp]
+   ["aws-amplify-react" :refer (withAuthenticator)]
+   ["/aws-exports.js" :default aws-exports]
+   ))
 
 
 (defn dev-setup []
   (when config/debug?
     (println "dev mode")))
 
+(def root-view
+  (reagent/adapt-react-class
+   (withAuthenticator
+    (reagent/reactify-component views/main-panel) true)))
+
 (defn ^:dev/after-load mount-root []
   (re-frame/clear-subscription-cache!)
-  (reagent/render [views/main-panel]
+  (.configure Amplify aws-exports)
+  (re-frame/dispatch-sync [::events/initialize-db])
+  (reagent/render [root-view]
                   (.getElementById js/document "app")))
 
 (defn init []
-  (re-frame/dispatch-sync [::events/initialize-db])
   (dev-setup)
   (mount-root))
